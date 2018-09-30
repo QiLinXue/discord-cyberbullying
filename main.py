0930f0c1e27ed31eabdac00df0e7f8091857ea87
QiLinXue <645367@pdsb.net>
2018-09-30 16:42:57 -0400

    create

---
+++ main.py
@@ -0,0 +1,36 @@
+# Discord Imports
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+# File Imports
+from _env import TOKEN
+
+bot = commands.Bot(command_prefix='#')
+
+@bot.event
+async def on_ready():
+    print ("Ready when you are xd")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! xSSS")
+    print ("user has pinged")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    await bot.say("The users name is: {}".format(user.name))
+    await bot.say("The users ID is: {}".format(user.id))
+    await bot.say("The users status is: {}".format(user.status))
+    await bot.say("The users highest role is: {}".format(user.top_role))
+    await bot.say("The user joined at: {}".format(user.joined_at))
+
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+    await bot.kick(user)
+
+bot.run(TOKEN)
