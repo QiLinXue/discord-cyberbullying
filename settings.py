79637e0d97cea1c5c61992f7ec5711726b308a00
QiLinXue <645367@pdsb.net>
2018-10-01 22:16:16 -0400

    no need

--- settings.py
+++
@@ -1,7 +0,0 @@
-# -------------------------------
-# --------- settings.py ---------
-# -------------------------------
-
-# Allow read access to .env file
-from dotenv import load_dotenv
-load_dotenv(verbose=True)
