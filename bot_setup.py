20601085c4457b3300fd34f39ce8a206eeb1342e
QiLinXue <645367@pdsb.net>
2018-10-01 22:15:37 -0400

    setup universal bot

---
+++ bot_setup.py
@@ -0,0 +1,11 @@
+# Discord Imports
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+client = Bot('') # Initialize
