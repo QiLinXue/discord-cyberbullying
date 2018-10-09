21b7b96a1814d7271b9f02f82da6cec778e20254
QiLinXue <645367@pdsb.net>
2018-10-08 20:02:33 -0400

    allows db to be run continuously

--- server/functions/badWords.py
+++ server/functions/badWords.py
@@ -1,24 +1,34 @@
 import mysql.connector
 from serverSetup import DBUSER,DBPASS
 
-mydb = mysql.connector.connect(
-    host="us-cdbr-iron-east-01.cleardb.net",
-    user=DBUSER,
-    passwd=DBPASS,
-    database="heroku_5e695080c7ef107"
-)
-
-mycursor = mydb.cursor()
-
 def run(badWord):
+
+    mydb = mysql.connector.connect(
+        host="us-cdbr-iron-east-01.cleardb.net",
+        user=DBUSER,
+        passwd=DBPASS,
+        database="heroku_5e695080c7ef107"
+    )
+
+    mycursor = mydb.cursor()
 
     sqlFormula = "INSERT INTO badwords (word, badness) VALUE (%s,%s)"
     word = (badWord,1)
 
     mycursor.execute(sqlFormula, word)
     mydb.commit()
+    mycursor.close()
 
 def fetch():
+
+    mydb = mysql.connector.connect(
+        host="us-cdbr-iron-east-01.cleardb.net",
+        user=DBUSER,
+        passwd=DBPASS,
+        database="heroku_5e695080c7ef107"
+    )
+
+    mycursor = mydb.cursor()
 
     sqlFormula = "SELECT * FROM badwords"
     mycursor.execute(sqlFormula)
@@ -29,4 +39,5 @@ def fetch():
     for row in myresults:
         badWordArray.append(row[0])
 
+    mycursor.close()
     return badWordArray
